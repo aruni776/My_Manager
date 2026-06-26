dc60605327badce27f8f768e3c011d7c41317acb
Jatin Choudhary <225982315+JatinChoudhary-07@users.noreply.github.com>
2026-06-26 14:44:28 +0530

    Fix database connection and resolve merge issues

--- database/db.py
+++ database/db.py
@@ -1,10 +1,8 @@
 import sqlite3
 
-def connect():
+def get_connection():
 
-    conn = sqlite3.connect(
+    return sqlite3.connect(
         "student_os.db",
         check_same_thread=False
     )
-
-    return conn
